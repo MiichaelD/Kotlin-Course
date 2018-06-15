4a35d9a1b6d85fa199b3a8dac42a49e29d6156b6
Michael Duarte <michael.duarte@hotmail.com>
2018-06-14 19:58:38 -0700

    Adds Main and metadata files

---
+++ HelloKotlin/src/aquarium/main.kt
@@ -0,0 +1,26 @@
+package aquarium
+
+fun main(args: Array<String>) {
+  buildAquarium();
+}
+
+fun buildAquarium() {
+  val myAquarium = Aquarium()
+
+  println("Length: ${myAquarium.length}, Width: ${myAquarium.width}, " +
+      "Height: ${myAquarium.height}")
+
+  myAquarium.height = 80
+
+  println("Height: ${myAquarium.height} cm")
+  println("Volume: ${myAquarium.volume} liters")
+
+  val smallAquarium = Aquarium(20, 15, 30)
+  println("Volume: ${smallAquarium.volume} liters")
+
+  val myAquarium2 = Aquarium(numberOfFish = 9)
+  println("Small Aquarium 2: ${myAquarium2.volume} liters with Length: ${myAquarium2.length}, " +
+      "Width: ${myAquarium2.width}, Height: ${myAquarium2.height}")
+
+  fishExample()
+}
