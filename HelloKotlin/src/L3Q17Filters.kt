b3ec1fe41958db91a09181d1b28cc02d55aa071a
Michael Duarte <michael.duarte@hotmail.com>
2018-06-14 19:58:19 -0700

    Updates filters indentation and format

--- HelloKotlin/src/L3Q17Filters.kt
+++ HelloKotlin/src/L3Q17Filters.kt
@@ -1,13 +1,16 @@
 fun main(args: Array<String>) {
   val spices= listOf("curry", "pepper", "cayenne", "ginger", "red curry", "green curry", "red pepper")
 
-  var filteredSpices = spices.asSequence().filter { it.contains("curry") }.sortedBy { it.length }
+  var filteredSpices = spices
+      .asSequence()
+      .filter { it.contains("curry") }
+      .sortedBy { it.length }
   println("Sorted curry spices: ${filteredSpices.toList()}")
 
-  filteredSpices = spices.asSequence().filter{it.startsWith('c')}
+  filteredSpices = spices.asSequence().filter { it.startsWith('c') }
   println("Spices that start with 'c': ${filteredSpices.toList()}")
 
-  filteredSpices = spices.asSequence().filter{it.startsWith('c') && it.endsWith('e')}
+  filteredSpices = spices.asSequence().filter { it.startsWith('c') && it.endsWith('e') }
   println("Spices that start with 'c' and end with 'e': ${filteredSpices.toList()}")
 
   filteredSpices = spices.take(3).asSequence().filter{it[0] == 'c' && it[it.length-1] == 'e'}
