a47f46d595d66d87a80a0f54d61ce8346460b3c8
Michael Duarte <michael.duarte@hotmail.com>
2018-06-21 22:32:33 -0700

    Switched SpiceColor#color form String to Color enum

--- HelloKotlin/src/spices/Spice.kt
+++ HelloKotlin/src/spices/Spice.kt
@@ -1,5 +1,12 @@
 package spices
 
+enum class Color(val rgb: Int) {
+  RED(0xFF0000),
+  GREEN(0x00FF00),
+  BLUE(0x0000FF),
+  YELLOW(0xFFFF00);
+}
+
 abstract class Spice (val name: String, val spiciness: String = "mild", color: SpiceColor):
     SpiceColor by color{
 
@@ -40,15 +47,15 @@ object defaultCurryGrinder: Grinder {
 }
 
 interface SpiceColor {
-  val color: String
+  val color: Color
 }
 
 object  RedSpiceColor: SpiceColor {
-  override val color = "red"
+  override val color = Color.RED
 }
 
 object  YellowSpiceColor: SpiceColor {
-  override val color = "yellow"
+  override val color = Color.YELLOW
 }
 
 class Curry(name: String = "Curry",
