8cdcc96250d6902e0f9c61c7761aeaf33c8c94a5
Michael Duarte <michael.duarte@hotmail.com>
2018-06-21 22:26:52 -0700

    Adds SpecialClasses.kt
    Object creates 1 instance, useful for implementing singletons
    
    enum classes
    
    sealed classes which can only be extended within the same file, compiler proveds more checks at compile time since it knows all the possible subclasses.

---
+++ HelloKotlin/src/SpecialClasses.kt
@@ -0,0 +1,27 @@
+
+
+object MobyDickWhale {
+  val author = "Herman Melville"
+
+  fun impl() {
+    // ...
+  }
+}
+
+enum class Color(val rgb: Int) {
+  RED(0xFF0000),
+  GREEN(0xFF0000),
+  BLUE(0xFF0000)
+}
+
+sealed class Seal
+
+class SeaLion: Seal()
+class Walrus: Seal()
+
+fun matchSeal(seal: Seal): String {
+  return when(seal) {
+    is Walrus -> "Walrus"
+    is SeaLion -> "Sea Lion"
+  }
+}
