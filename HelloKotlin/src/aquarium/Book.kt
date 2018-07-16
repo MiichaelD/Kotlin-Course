05fca352c46a046d1b53b03d1884a4724c429fa8
Michael Duarte <michael.duarte@hotmail.com>
2018-07-15 20:49:07 -0700

    Adds Extension Functions to Book

--- HelloKotlin/src/aquarium/Book.kt
+++ HelloKotlin/src/aquarium/Book.kt
@@ -1,6 +1,6 @@
 package aquarium
 
-open class Book(val title:String, val author:String) {
+open class Book(val title:String, val author:String, var pages: Int = 100) {
 
   companion object {
     const val MAX_BOOKS = 3;
@@ -33,4 +33,35 @@ class EBook(title:String, author:String, val format:String = "yrcy")
   override fun readPage() {
     wordCount += 250
   }
+}
+
+fun Book.getWeight(): Float {
+  return pages * 1.5f
+}
+
+fun Book.tornPages(pagesTorn: Int) {
+  pages -= pagesTorn
+  if (pages < 0) pages = 0
+}
+
+class Puppy {
+  fun playWithBook(book: Book) {
+    book.tornPages(Math.ceil(Math.random()*10).toInt())
+  }
+}
+
+fun staticPuppyTest() {
+  val book = Book("Romeo and Juliet", "William Shakespeare")
+  val puppy = Puppy()
+
+
+  while (book.pages > 0) {
+    println("Book: ${book.title} has ${book.pages} left, it weights ${book.getWeight()}")
+    puppy.playWithBook(book)
+  }
+  println("Book: ${book.title} has ${book.pages} left")
+}
+
+fun main(args: Array<String>) {
+  staticPuppyTest()
 }
