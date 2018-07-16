9c56b2def1efa5211b707c7d5075abe563b63229
Michael Duarte <michael.duarte@hotmail.com>
2018-07-15 20:51:30 -0700

    Merget Book.kt into aquarium/Book.kt

--- HelloKotlin/src/aquarium/Book.kt
+++ HelloKotlin/src/aquarium/Book.kt
@@ -1,6 +1,13 @@
 package aquarium
 
-open class Book(val title:String, val author:String, var pages: Int = 100) {
+open class Book(
+    val title:String, val author:String, val year:Int = 2000, var pages: Int = 100) {
+
+  private var currentPage = 1
+
+  open fun readPage() {
+    ++currentPage
+  }
 
   companion object {
     const val MAX_BOOKS = 3;
@@ -10,10 +17,12 @@ open class Book(val title:String, val author:String, var pages: Int = 100) {
     const val BASE_URL = "http://www.turtlecare.net/"
   }
 
-  private var currentPage = 1
+  fun getTitleAndAuthor(): Pair<String, String> {
+    return (title to author)
+  }
 
-  open fun readPage() {
-    ++currentPage
+  fun getTitleAuthorAndYear(): Triple<String, String, Int> {
+    return Triple(title, author, year)
   }
 
   fun canBorrow(hasBooks: Int): Boolean {
@@ -34,6 +43,33 @@ class EBook(title:String, author:String, val format:String = "yrcy")
     wordCount += 250
   }
 }
+
+fun staticTestCollections() {
+  val book = Book("X", "Y", 1991)
+  println("Here is your book '${book.title}' written by '${book.author}' in ${book.year}")
+
+  val shakespeare: String = "William Shakespeare"
+
+  val romeoAndJuliet = Book("Romeo and Juliet", shakespeare, 1597)
+  val twelfthNight = Book("Twelfth night", shakespeare, 1602)
+  val asYouLikeIt = Book("As you like it", shakespeare, 1603)
+  val othello = Book("Hamlet", shakespeare, 1604)
+  val hamlet = Book("Hamlet", shakespeare, 1609)
+  val wintersTale = Book("Winter's tale", shakespeare, 1611)
+
+
+  val allBooks:Set<Book> =
+      setOf(romeoAndJuliet, twelfthNight, asYouLikeIt, othello, hamlet, wintersTale)
+
+  val library:Map<String, Set<Book>> = mapOf((shakespeare to allBooks))
+  println("Is hamlet in the library? ${library.any { it.value.contains(hamlet) }}")
+
+  val moreBooks: MutableMap<String, String> = mutableMapOf("Wilhelm Tell" to "Schiller")
+  moreBooks.getOrPut("Jungle Book") { "Kipling" }
+  moreBooks.getOrPut("Hamlet") { "Shakespeare" }
+  println(moreBooks)
+}
+
 
 fun Book.getWeight(): Float {
   return pages * 1.5f
@@ -63,5 +99,6 @@ fun staticPuppyTest() {
 }
 
 fun main(args: Array<String>) {
+//  staticTestCollections()
   staticPuppyTest()
 }
