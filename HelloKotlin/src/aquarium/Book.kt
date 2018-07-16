d747f9bafc9f526b20f0eafec8d677f2f424fe01
Michael Duarte <michael.duarte@hotmail.com>
2018-07-15 20:33:10 -0700

    Adds constants to Book.kt

--- HelloKotlin/src/aquarium/Book.kt
+++ HelloKotlin/src/aquarium/Book.kt
@@ -2,10 +2,26 @@ package aquarium
 
 open class Book(val title:String, val author:String) {
 
+  companion object {
+    const val MAX_BOOKS = 3;
+  }
+
+  object Constants {
+    const val BASE_URL = "http://www.turtlecare.net/"
+  }
+
   private var currentPage = 1
 
   open fun readPage() {
     ++currentPage
+  }
+
+  fun canBorrow(hasBooks: Int): Boolean {
+    return hasBooks < MAX_BOOKS
+  }
+
+  fun printUrl() {
+    println(Constants.BASE_URL + title + ".html")
   }
 }
 
