190eba436f94b5b38f83c63c478a7dc4089fbaea
Michael Duarte <michael.duarte@hotmail.com>
2018-07-15 21:12:51 -0700

    Adds Generics In and Out to Aquarium.kt

--- HelloKotlin/src/aquarium/Aquarium.kt
+++ HelloKotlin/src/aquarium/Aquarium.kt
@@ -2,8 +2,7 @@ package aquarium
 
 import kotlin.math.PI
 
-
-open class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+open class Aquarium<out T: WaterSupply>(val waterSupply: T, var length: Int = 100, var width: Int = 20, var height: Int = 40) {
 
   open var volume: Int
       get() = width * height * length / 1000
@@ -16,13 +15,59 @@ open class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int 
     val tank:Double = water * 1.1
     height = (tank / (length * width)).toInt()
   }
+
+  fun addWater(cleaner: Cleaner<T>) {
+    if(waterSupply.needsProcessed) {
+      cleaner.clean(waterSupply)
+    }
+    check(!waterSupply.needsProcessed) { "water supply needs to be processed"}
+
+    println("adding water from $waterSupply")
+  }
 }
 
-class TowerAquarium() : Aquarium() {
+class TowerAquarium() : Aquarium<FishStoreWater>(FishStoreWater) {
 
   override var volume: Int
       get() = (width * height * length / 1000 * PI).toInt()
       set(value) { height = (value * 1000) / (width * length) }
 
   override var water = volume * 0.8
+}
+
+open class WaterSupply(var needsProcessed: Boolean)
+
+object TapWater: WaterSupply(true) {
+  fun addChemicalCleaners() = apply {needsProcessed = false}
+}
+
+object FishStoreWater: WaterSupply(false)
+
+object LakeWater: WaterSupply(true) {
+  fun filter() = apply {needsProcessed = false}
+}
+
+interface Cleaner<in T: WaterSupply> {
+  fun clean(waterSupply: T)
+}
+
+class TapWaterCleaner: Cleaner<TapWater> {
+  override fun clean(waterSupply: TapWater) {
+    waterSupply.addChemicalCleaners()
+  }
+}
+
+class LakeWaterCleaner: Cleaner<LakeWater> {
+  override fun clean(waterSupply: LakeWater) {
+    waterSupply.filter()
+  }
+}
+
+fun addItemTo(aquarium: Aquarium<WaterSupply>) = println("item added")
+
+fun genericExample() {
+  val cleaner = TapWaterCleaner()
+  val aquarium = Aquarium(TapWater)
+  addItemTo(aquarium) // only works if T is marked as "out" to tell compiler to avoid static check
+  aquarium.addWater(cleaner)
 }
